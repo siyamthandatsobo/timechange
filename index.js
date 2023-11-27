9bc958f78fca97538c32087316840276b71ee5f9
Siyamthanda Tsobo <siyamthandatsobo@gmail.com>
2023-11-27 11:23:48 +0200

    modified text

--- index.js
+++ index.js
@@ -3,14 +3,29 @@ let sec = document.getElementById('Sec');
 let minutes = document.getElementById('Min').value 
 
 
+
+
+
+
+
 Convert.addEventListener('click', function() {
     let min = document.getElementById('Min').value
 
    minToSec(minutes);
 
 })
-// this does the sum to make that minutes converts to seconds
+
+
+
+
+
+
 function minToSec(num) {
+
+
     let sum = num * 60 ;
+
+
+
 sec.value = sum
 }
